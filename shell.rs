7ebf83c0b677782327089d9c84f1f04fecc80b83
Robert Lord <robert@lord.io>
2014-04-10 16:32:55 -0400

    Add basic REPL

--- shell.rs
+++ shell.rs
@@ -1,13 +1,37 @@
 use std::io::timer;
 use std::io::stdio;
 
+struct Shell {
+    cmd_prompt: ~str,
+}
+
+impl Shell {
+    fn new(prompt_str: &str) -> Shell {
+        Shell {
+            cmd_prompt: prompt_str.to_owned(),
+        }
+    }
+
+    fn start(&mut self) {
+        let mut stdin = stdio::stdin();
+        loop {
+            stdio::print(self.cmd_prompt);
+            stdio::flush();
+
+            let line = stdin.read_line().unwrap();
+            let cmd_line = line.trim().to_owned();
+            let program = cmd_line.splitn(' ', 1).nth(0).expect("no program");
+
+            match program {
+                ""      =>  { continue; }
+                "\u274c"  =>  { return; }
+                // _       =>  { self.line(cmd_line); }
+                _ => { println!("You just ran {}", cmd_line) }
+            }
+        }
+    }
+}
+
 fn main() {
-  stdio::print("Loading...");
-  stdio::flush();
-  timer::sleep(1000);
-  for num in range(0, 1000) {
-    stdio::print("\r\x1b[K" + num.to_str());
-    stdio::flush();
-    timer::sleep(100);
-  }
+    Shell::new("\U0001f41a \U0001f4b2 ").start();
 }

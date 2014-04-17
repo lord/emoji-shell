2880f5d6bed8441c5f52e41a62f14fcb3e777695
Robert Lord <robert@lord.io>
2014-04-17 14:18:14 -0400

    Add running commands and check to see if command exists

--- shell.rs
+++ shell.rs
@@ -1,5 +1,6 @@
-use std::io::timer;
 use std::io::stdio;
+use std::io::Process;
+use std::io::process;
 
 struct Shell {
     cmd_prompt: ~str,
@@ -25,13 +26,46 @@ impl Shell {
             match program {
                 ""      =>  { continue; }
                 "\u274c"  =>  { return; }
-                // _       =>  { self.line(cmd_line); }
-                _ => { println!("You just ran {}", cmd_line) }
+                _       =>  { self.cmd(cmd_line.clone()); }
+                // _ => { println!("You just ran {}", cmd_line) }
             }
+        }
+    }
+
+    fn cmd(&mut self, cmd_line: ~str) {
+        let mut arg_vec = ~[];
+        let mut cmd = ~"";
+        for (index, s) in cmd_line.split_str(" ").enumerate() {
+            if index == 0 { cmd = s.to_owned(); } else { arg_vec.push(s.to_owned()); }
+        }
+        self.run_cmd(cmd, arg_vec);
+    }
+
+    fn run_cmd(&mut self, cmd: ~str, argv: ~[~str]) {
+        if self.cmd_exists(cmd) {
+            println!("Executing {}", cmd);
+            let p = Process::new(cmd, argv);
+            let status = p.unwrap().wait();
+            match status {
+                process::ExitSignal(st) => println!("Exited with signal {}", st),
+                process::ExitStatus(st) => println!("Exited with status {}", st)
+            }
+        }
+        else {
+            println!("Command {} not found.", cmd)
+        }
+    }
+
+    fn cmd_exists(&mut self, cmd: &str) -> bool {
+        let p = Process::new("which", [cmd.to_owned()]);
+        let status = p.unwrap().wait();
+        match status {
+            process::ExitSignal(st) => st == 0,
+            process::ExitStatus(st) => st == 0
         }
     }
 }
 
 fn main() {
-    Shell::new("\U0001f41a \U0001f4b2 ").start();
+    Shell::new("\U0001f41a  ").start();
 }
